18c1d68c3781d2c9f40d027150aaf72882bb9181
git-ming <git-ming@bitbucket.com>
2015-08-22 22:52:34 +0800

    登陆和注册

---
+++ resource/blog/js/login.js
@@ -0,0 +1,23 @@
+$(function(){
+    checkNull($('#login'));
+});
+function checkNull(_this){
+    $(_this).find('.input-group').append('<em>不得为空</em>');
+    $(_this).find('.input-group input').css('position','relative')
+    $(_this).find('em').css({
+        'position':'absolute',
+        'top':'5px',
+        'right':'5px',
+        'color':'red',
+        'font-style':'normal',
+        'z-index':99,
+        'display':'none'
+    });
+    $(_this).find('input[type=text]').blur(function(){
+        if($(this).val()){
+
+        }else{
+
+        }
+    });
+};
