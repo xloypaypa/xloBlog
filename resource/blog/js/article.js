f5724c6bdf6297e0cd7feaa7cf0a019e7eff1ca3
git-ming <threehxhot@163.com>
2015-09-08 14:53:15 +0800

    中文转码

--- resource/blog/js/article.js
+++ resource/blog/js/article.js
@@ -4,29 +4,44 @@ $(function(){
     };
     ajaxRequest('/getDocument',data,function(response){
         var date=transformDate(response.time.$date);
-        $('.article-content h2 a').html(response.title);
-        $('.author').html(response.author);
-        $('.article-body').html(response.body);
+        var commentLen=response.reply?response.reply.length:0;
+        var title=decodeURIComponent(response.title);
+        var body=decodeURIComponent(response.body);
+        var author=decodeURIComponent(response.author);
+        $('.article-content h2 a').html(title);
+        $('.author').html(author);
+        $('.article-body').html(body);
         $('.date').html(date);
-        $('.commentNum').html(response.reply.length);
-        for(var i=0;i<response.reply.length;i++){
+        $('.commentNum').html(commentLen);
+        for(var i=0;i<commentLen;i++){
             $('.comments-list').append("<li class='list-group-item'>"+$('#comment-list').html()+"</li>");
-            var commentDate=transformDate(response.reply[i].data.$date);
             var commentNo=$('.comments-list li').eq(i);
-            commentNo.find('.observer a').html(response.reply[i].author);
+            var replyNo=response.reply[i];
+            var commentDate=transformDate(replyNo.data.$date);
+            var observer=decodeURIComponent(replyNo.author);
+            var commentContent=decodeURIComponent(replyNo.reply);
+            commentNo.find('.observer a').html(observer);
             commentNo.find('.comment-date').html(commentDate);
-            commentNo.find('.comment-content p').html(response.reply[i].reply);
+            commentNo.find('.comment-content p').html(commentContent);
         }
     });
     $('.myComment .submit').click(function(){
-        var comment=$('.myComment textarea').val();
-        console.log(comment);
-        var data={
-            id:getQueryString('id'),
-            reply:comment
+        var comment=encodeURIComponent($('.myComment textarea').val());
+        if(comment){
+            var data={
+                id:getQueryString('id'),
+                reply:comment
+            };
+            ajaxHeader('/reply',data,function(response){
+                if(response.return==200){
+                    location.reload();
+                }else{
+                    alert('评论失败');
+                }
+            });
+        }else{
+            alert('不得为空');
         }
-        ajaxHeader('/reply',data,function(response){
-            console.log(response);
-        });
+
     });
 });
--- resource/blog/js/editor.js
+++ resource/blog/js/editor.js
@@ -1,9 +1,8 @@
 $(function(){
     $('.submit').click(function(){
-        console.log($('.blog-content textarea').val());
         var data={
-            title:$('.blog-title input').val(),
-            body:$('.blog-content textarea').val()
+            title:encodeURIComponent($('.blog-title input').val()),
+            body:encodeURIComponent($('.blog-content textarea').val())
         };
         ajaxHeader('/addDocument',data,function(response){
             if(response.return==200){
