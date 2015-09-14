a1e84dedb716d032d47a6e50618ae376c3bf48b0
xloypaypa <245532675@qq.com>
2015-09-14 17:54:41 +0800

    remove unnecessary solver

--- main/java/net/tool/LengthLimitReadServerSolver.java
+++ main/java/net/tool/LengthLimitReadServerSolver.java
@@ -7,6 +7,7 @@ import model.tool.streamConnector.StreamConnector;
 import model.tool.streamConnector.io.LengthLimitStreamIONode;
 import model.tool.streamConnector.io.StreamIONode;
 import net.server.serverSolver.RequestSolver;
+import net.server.serverSolver.normalServer.NormalKeepAliveServerSolver;
 import net.tool.connection.event.ConnectionEvent;
 import net.tool.connection.event.ConnectionEventManager;
 
@@ -14,16 +15,21 @@ import net.tool.connection.event.ConnectionEventManager;
  * Created by xlo on 2015/8/24.
  * it's length limit read server solver
  */
-public abstract class LengthLimitReadServerSolver extends ReadServerSolver {
+public abstract class LengthLimitReadServerSolver extends NormalKeepAliveServerSolver {
     protected String message;
     protected byte[] originalMessage;
     protected NormalByteIO byteIO;
     protected long length;
 
     public LengthLimitReadServerSolver() {
-        ConnectionEventManager.getConnectionEventManager().addEventHandlerToItem(ConnectionEvent.connectEnd, this,
+        ConnectionEventManager.getConnectionEventManager().addEventHandlerToItem(ConnectionEvent.connectSuccess, this,
                 (event, solver) -> {
                     trySolveMessage();
+                    if (byteIO != null) byteIO.close();
+                });
+        ConnectionEventManager.getConnectionEventManager().addEventHandlerToItem(ConnectionEvent.connectFail, this,
+                (event, solver) -> {
+                    closeSocket();
                     if (byteIO != null) byteIO.close();
                 });
     }
@@ -74,6 +80,10 @@ public abstract class LengthLimitReadServerSolver extends ReadServerSolver {
         }
 
         LogManager.getLogManager().writeLog("blog read", this.message);
+    }
+
+    @Override
+    public void startNextServerSolver() {
     }
 
     public LengthLimitReadServerSolver setRequestSolver(RequestSolver requestSolver) {
