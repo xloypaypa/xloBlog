429c78c64790b5b3b42b828075bfc304e3d23627
git-ming <threehxhot@163.com>
2015-09-17 16:42:37 +0800

    文章截取

--- resource/blog/js/editor.js
+++ resource/blog/js/editor.js
@@ -2,9 +2,12 @@ $(function(){
     $('.nickName').html(decodeURIComponent(window.username));
     $('.submit').click(function(){
         console.log(encodeURIComponent($('.blog-title input').val()));
+        var value=$('.blog-content textarea').val();
+        var preview=encodeURIComponent(value.substr(0,100));
         var data={
             title:encodeURIComponent($('.blog-title input').val()),
-            body:encodeURIComponent($('.blog-content textarea').val())
+            body:encodeURIComponent(value),
+            preview:preview.length
         };
         ajaxHeader('/addDocument',data,function(data){
                 location.href='index.html';
