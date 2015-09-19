49172e0380a1507acdc193f38133b29089240419
git-ming <threehxhot@163.com>
2015-09-19 20:08:22 +0800

    嵌入markdown

--- resource/blog/js/editor.js
+++ resource/blog/js/editor.js
@@ -1,4 +1,6 @@
 $(function(){
+
+    markdown();
     $('.nickName').html(decodeURIComponent(window.username));
     $('.submit').click(function(){
         var value=$('.blog-content textarea').val();
@@ -13,3 +15,46 @@ $(function(){
         });
     });
 });
+function markdown(){
+    var opts = {
+        container: 'epiceditor',
+        textarea: null,
+        basePath: 'epiceditor',
+        clientSideStorage: true,
+        localStorageName: 'epiceditor',
+        useNativeFullscreen: true,
+        parser: marked,
+        file: {
+            name: 'epiceditor',
+            defaultContent: '',
+            autoSave: 100
+        },
+        theme: {
+            base: '../../../libs/EpicEditor/themes/base/epiceditor.css',
+            preview: '../../../libs/EpicEditor/themes/preview/github.css',
+            editor: '../../../libs/EpicEditor/themes/editor/epic-light.css'
+        },
+        button: {
+            preview: true,
+            fullscreen: true,
+            bar: "auto"
+        },
+        focusOnLoad: false,
+        shortcut: {
+            modifier: 18,
+            fullscreen: 70,
+            preview: 80
+        },
+        string: {
+            togglePreview: 'Toggle Preview Mode',
+            toggleEdit: 'Toggle Edit Mode',
+            toggleFullscreen: 'Enter Fullscreen'
+        },
+        autogrow: false
+    };
+    var editor = new EpicEditor(opts).load();
+
+    $('.submit').click(function () {
+        console.log(editor.getElement('editor').body.innerHTML); // Returns the editor's content
+    });
+}
