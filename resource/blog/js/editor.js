1857fd28b0d57110f292dec8fa074bbe63804f83
git-ming <threehxhot@163.com>
2015-09-20 22:31:02 +0800

    顶部的工具栏还没法用

--- resource/blog/js/editor.js
+++ resource/blog/js/editor.js
@@ -2,18 +2,7 @@ $(function(){
 
     markdown();
     $('.nickName').html(decodeURIComponent(window.username));
-    $('.submit').click(function(){
-        var value=$('.blog-content textarea').val();
-        var preview=encodeURIComponent(value.substr(0,100));
-        var data={
-            title:encodeURIComponent($('.blog-title input').val()),
-            body:encodeURIComponent(value),
-            preview:preview.length
-        };
-        ajaxHeader('/addDocument',data,function(data){
-                location.href='index.html';
-        });
-    });
+
 });
 function markdown(){
     var opts = {
@@ -55,6 +44,25 @@ function markdown(){
     var editor = new EpicEditor(opts).load();
 
     $('.submit').click(function () {
-        console.log(editor.getElement('editor').body.innerHTML); // Returns the editor's content
+        var sendVal=editor.getElement('editor').body.innerHTML;
+        var preview=encodeURIComponent(sendVal.slice(0,99));
+        var title=encodeURIComponent($('.blog-title input').val());
+        addDocument(title,encodeURIComponent(sendVal),preview);
+        editor.focus();
+    });
+    $('.su-tool-code').click(function(){
+
+    });
+
+}
+
+function addDocument(title,body,preview){
+    var data={
+        title:title,
+        body:body,
+        preview:preview.length
+    };
+    ajaxHeader('/addDocument',data,function(data){
+        location.href='index.html';
     });
 }
